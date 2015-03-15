b856b3dec3a0fad323b57d77960877d4d8e74f37
kirin <a29243685@msn.com>
2015-03-15 12:55:02 +0800

    this create animition and bundle wall

---
+++ CatchingMan/Assets/Script/Player.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static float 
+
+	private Vector3 StartPos, EndPos;
+	private bool _IsMouseDown = false;
+	private GameObject bucket;
+	private bool isMoveRight = true;
+
+	// Use this for initialization
+	void Start () {
+		bucket = GameObject.Find ("woodbucket");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+
+			StartPos = Input.mousePosition;
+			_IsMouseDown = true;
+		}
+		if(Input.GetMouseButtonUp(0)){
+			_IsMouseDown = false;
+		}
+		if(_IsMouseDown){
+			EndPos = Input.mousePosition;
+			DetectGuesture();
+		}
+
+		updatePlayerMoveStyle ();
+	}
+
+	private void updatePlayerMoveStyle(){
+		Vector3 scale = transform.localScale;
+		bool needChangetRight = scale.x < 0 && isMoveRight;
+		bool needChangetLeft = scale.x > 0 && !isMoveRight;
+		if (needChangetLeft || needChangetRight) {
+			scale.x *= -1f;		
+			transform.localScale = scale;
+		}
+	}
+
+	private void DetectGuesture(){
+		Vector3 p1 = Camera.main.ScreenToWorldPoint (StartPos);
+		Vector3 p2 = Camera.main.ScreenToWorldPoint (EndPos);
+		float len = (p2.x - p1.x)*2;
+
+		Vector3 p3 = new Vector3 (transform.position.x+len,transform.position.y,transform.position.z);
+		float w = (Camera.main.orthographicSize * Camera.main.aspect) - (bucket.renderer.bounds.size.x / 2);
+
+		p3.x = Mathf.Max (p3.x, -w);
+		p3.x = Mathf.Min (p3.x, w);
+		transform.position = p3;
+		StartPos = EndPos;
+
+		updateMoveFlag (len);
+
+	}
+
+	private void updateMoveFlag(float len){
+		if (Mathf.Abs (len) > 0.9f) {
+			if(len > 0){
+				isMoveRight = true;
+			}else if(len <0){
+				isMoveRight = false;
+			}
+		}
+	}
+}
--- CatchingMan/Assets/Script/Scene.cs
+++ CatchingMan/Assets/Script/Scene.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Scene : MonoBehaviour {
 
+	public GameObject Wall;
+
 	// Use this for initialization
 	void Start () {
-	
+		SetupWall();
 	}
 	
 	// Update is called once per frame
@@ -13,5 +15,17 @@ public class Scene : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Escape)){
 			Application.Quit();
 		}
+	}
+
+	private void SetupWall(){
+
+		//GameObject Wall = GameObject.Find ("Wall");
+		Debug.Log (Wall);
+		float Distance = Camera.main.orthographicSize * Camera.main.aspect * 1.1f;
+		GameObject leftWall = Instantiate(Wall) as GameObject;
+		leftWall.transform.position = new Vector3 (-Distance,0,0);
+		GameObject rightWall = Instantiate(Wall) as GameObject;
+		rightWall.transform.position = new Vector3 (Distance,0,0);
+		
 	}
 }
